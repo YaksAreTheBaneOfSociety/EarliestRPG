17d5280bc8cabba4b6f0a71746c4f5017a0c901c
YaksAreTheBaneOfSociety <73199023+YaksAreTheBaneOfSociety@users.noreply.github.com>
2024-03-04 15:08:49 -0600

    add word of the day

--- BOT_XP_BOSS/index.js
+++ BOT_XP_BOSS/index.js
@@ -14,6 +14,8 @@ const client = new Client({
 const fs = require("fs")
 const path = require("path")
 
+var cron = require('node-cron');
+
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -56,11 +58,21 @@ client.on(Events.InteractionCreate, async interaction => {
 
 let xpData = fs.readFileSync("xp.json") // reads the json file
 let xpObject = JSON.parse(xpData) // turns json into js
+let randomWordsListData = fs.readFileSync("wordsList.json") // reads the json file
+let randomWordsList = JSON.parse(randomWordsListData) // turns json into js
+let randomWordData = fs.readFileSync("wordOfTheDay.json") // reads the json file
+let randomWord = JSON.parse(randomWordData) // turns json into js
 
 const xpMin = 15
 const xpMax = 40
 const xpMultiplierForNotRobot = 2
+let newRandomWord = {}
+let oldRandomWord = {}
 
+var getRandomWord = function(wordList){
+    const randomIndex = Math.floor(Math.random() * wordList.length);
+    return wordList[randomIndex];
+}
 function levelUpCheck(xpObject,xpIndex,m){
 	xpNeeded=75+100*xpObject[xpIndex].level
 	if(xpObject[xpIndex].xp>=xpNeeded){
@@ -69,12 +81,37 @@ function levelUpCheck(xpObject,xpIndex,m){
 		client.channels.cache.get('1182165246870310984').send(`${m.author} has reached level **${xpObject[xpIndex].level}**`)
 	}
 }
+function selectwordoftheday(){
+	newRandomWord = getRandomWord(randomWordsList)
+	newRandomWord.found = false
+	if(randomWord[0] != null){
+		oldRandomWord = randomWord[0]
+		randomWord[1] = oldRandomWord
+	}
+	randomWord[0] = newRandomWord
+	let jsonRandomWord = JSON.stringify(randomWord)
+	fs.writeFileSync("wordOfTheDay.json", jsonRandomWord)
+	if(randomWord[1] != null){
+		client.channels.cache.get('1182165246870310984').send(`Yesterday's random word was **${randomWord[1].word}**`)
+	}
+	client.channels.cache.get('1182165246870310984').send(`Today's random word has been selected! it has been used **${newRandomWord.count}** times (before March 4, 2024)`)
 
+}
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	
+	cron.schedule('3 15 * * *', () => {
+		selectwordoftheday()
+	},{
+		scheduled: true,
+		timezone: "US/Central"
+	  });
+	if(randomWord[0] == null){
+		selectwordoftheday()
+	}
 });
 
 client.on("messageCreate", (m) => {
@@ -96,6 +133,13 @@ client.on("messageCreate", (m) => {
 		}
 	}catch{
 		xpObject[xpIndex].username = m.author.username
+	}
+	if(m.toString().includes(newRandomWord.word) && newRandomWord.found == false){
+		newRandomWord.found = true
+		let xpToAdd = 25*Math.floor(Math.random()*(xpMax-xpMin+1))+xpMin
+		client.channels.cache.get('1182165246870310984').send(`${m.author} found the secret word: **${newRandomWord.word}** and was awarded ${xpToAdd} xp`)
+		xpObject[xpIndex].xp += xpToAdd
+		levelUpCheck(xpObject,xpIndex,m)
 	}
 	if(xpObject[xpIndex].timeout == false){
 		let xpToAdd = Math.floor(Math.random()*(xpMax-xpMin+1))+xpMin
